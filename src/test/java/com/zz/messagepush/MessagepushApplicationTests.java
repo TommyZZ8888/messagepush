767b65fc0ce896302e97c1bfbd11b0c142f875a0
TommyZZ8888 <1950442495@qq.com>
2023-03-07 17:33:43 +0800

    github austin create

---
+++ src/test/java/com/zz/messagepush/MessagepushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zz.messagepush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagepushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
